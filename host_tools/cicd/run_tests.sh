7dcb59f11223adad7f94294b0d40542a596cd227
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2020-05-03 23:44:54 -0400

    Skip test in run_tests.sh if a test fails to build

--- host_tools/cicd/run_tests.sh
+++ host_tools/cicd/run_tests.sh
@@ -6,6 +6,11 @@ do
 	echo "Executing test ${test}..."
 	echo "   include 'testsuite/tests/${test}.asm'" > testsuite/current_test.asm
 	make -B testsuite > /dev/null
+	passed=`echo $?`
+	if (( passed != 0 )); then
+		echo "Failed to compile!"
+		continue
+	fi
 	mame_output=`xvfb-run mame genesis -debug -debugscript testsuite/build.mds -cart out.bin -video none -seconds_to_run 5 -oslog`
 	passed=`echo ${mame_output} | grep "test<passed>"`
 	passed=`echo $?`
