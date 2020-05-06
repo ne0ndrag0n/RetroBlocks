e7be0670b773075837be131b210d0a33288dcebd
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2020-05-06 18:24:37 -0400

    CI/CD Enhancements: Unit Testing

--- host_tools/cicd/run_tests.sh
+++ host_tools/cicd/run_tests.sh
@@ -13,13 +13,13 @@ do
 		echo -e "\e[91mFailed to compile\e[0m ${test}!"
 		echo "$makeresult"
 		echo ""
-		let index++
 		continue
 	fi
 	echo -en "\e[93m[....]\e[0m"
 	mame_output=`xvfb-run mame genesis -debug -debugscript testsuite/build.mds -cart out.bin -video none -seconds_to_run 5 -oslog`
-	passed=`echo ${mame_output} | grep "test<passed>" | echo $?`
-	if (( passed == 0 )); then
+	passed=`echo ${mame_output} | grep "test<passed>"`
+	result_code=`echo $?`
+	if (( result_code == 0 )); then
 		passed='\e[92m[PASS]\e[0m\t'
 	else
 		passed='\e[91m[FAIL]\e[0m\t'
@@ -29,5 +29,24 @@ do
 	echo -en "\r        \r"
 	echo -e $passed
 
-	let index++
+	# Log error if it was indicated
+	if (( result_code == 1 )); then
+		# Stringbuild from mame_output and stringbuilder<%d> output
+		grepped=`echo "$mame_output" | grep -Po "stringbuilder<\d+>"`
+		symbols=($grepped)
+		result=""
+		for symbol in "${symbols[@]}"
+		do
+			symbol=`echo ${symbol} | sed -e 's/^stringbuilder<//' -e 's/>$//'`
+			if [[ $symbol = '10' ]]; then
+				result="$result\n"
+			else
+				as_ascii=`printf "\x$(printf %x ${symbol})"`
+				result="$result$as_ascii"
+			fi
+		done
+		if [[ $result != "" ]]; then
+			echo -e $result
+		fi
+	fi
 done
--- testsuite/constants.asm
+++ testsuite/constants.asm
@@ -2,6 +2,31 @@
 TESTSUITE_CONSTANTS = 1
 
 TESTSUITE_CONTROL_BYTE = $A14102
-TESTSUITE_DATA_BYTE    = $A14103
+TESTSUITE_ANNOTATION   = $A14103
+
+	macro TestsuiteEmulatorExit
+		move.b	#1, TESTSUITE_CONTROL_BYTE
+	endm
+
+	macro TestsuiteReportPass
+		move.b	#2, TESTSUITE_CONTROL_BYTE
+	endm
+
+	macro TestsuiteReportFail
+		move.b	#3, TESTSUITE_CONTROL_BYTE
+	endm
+
+	macro TestsuitePrint
+		move.l	#\1, a0
+
+	TestsuitePrint_Loop\@:
+		tst.b	(a0)
+		beq.s	TestsuitePrint_End\@
+
+		move.b	(a0)+, TESTSUITE_ANNOTATION
+		bra.s TestsuitePrint_Loop\@
+
+	TestsuitePrint_End\@:
+	endm
 
 	endif
