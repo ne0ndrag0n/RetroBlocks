2d57be898aac7e03f68f4953ed63fa68812f4f2a
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2021-05-25 01:53:35 -0400

    Remove old isometric tiles - will be doing 3d rasterisation

--- data/patterns/isotiles/mod.asm
+++
@@ -1,6 +0,0 @@
-	ifnd H_PATTERNS_ISOTILES_MOD = 1
-H_PATTERNS_ISOTILES_MOD = 1
-
-	include 'data/patterns/isotiles/default.asm'
-
-	endif
--- data/patterns/mod.asm
+++ data/patterns/mod.asm
@@ -1,7 +1,6 @@
   ifnd H_DATA_PATTERNS
 H_DATA_PATTERNS = 1
 
-  include 'data/patterns/isotiles/mod.asm'
   include 'data/patterns/font.asm'
   include 'data/patterns/titlescreen.asm'
 
