75998659d54818fb98d4b4eaa6642445accba4e1
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2021-09-01 22:50:08 -0400

    Untested shellsort

--- modules/helpers/sort.asm
+++ modules/helpers/sort.asm
@@ -7,6 +7,9 @@ H_HELPERS_SORT = 1
 Shellsort:
 	SetupFramePointer
 	move.l	a2, -(sp)
+	move.l	a3, -(sp)
+	move.l	a4, -(sp)
+	move.l	d2, -(sp)
 
 	cmpi.w	#2,	10(fp)
 	blo		Shellsort_End	; nothing to do for items less than 2
@@ -14,23 +17,20 @@ Shellsort:
 	; while size != 0
 	; 	divide size by 2
 	;	for i = 0 to size - 1
-	;		cursor = address + i
+	;		start = address + i
+	;		cursor = start + size
 	;		loop
-	;			if cursor + size < end
-	;				cursor += size
-	;			else
-	;				break
-	;		loop
-	;			if cursor - size >= address
-	;				left = cursor - size
-	;				if *left > *cursor
-	;					swap *left and *cursor
-	;				cursor = left
+	;			origin = cursor
+	;			while cursor > start && *cursor < *(cursor - size)
+	;				swap *cursor and *(cursor - size)
+	;				cursor = cursor - size
+	;			if origin + size < end
+	;				cursor = origin + size
 	;			else
 	;				break
 
-	move.l	4(fp), a1
-	add.l	8(fp), a1				; a1 = end = address + size
+	move.l	4(fp), a2
+	add.l	8(fp), a2				; a2 = end = address + size
 
 Shellsort_While_SizeNot0:
 	move.w	10(fp), d0
@@ -40,49 +40,55 @@ Shellsort_While_SizeNot0:
 	move.w	d0, 10(fp)				; size / 2
 
 	move.l	#0, d1					; d1 = i = 0
-Shellsort_For_0ToSizeMinus1:
-	move.l	4(fp), a0
-	add.l	d1, a0					; a0 = cursor = address + i
+Shellsort_OuterFor:
+	move.l	4(fp), a1
+	add.l	d1,	a1					; a1 = start = address + i
 
-Shellsort_For_Loop1:
-	move.l	8(fp), d0
-	add.l	a0, d0
-	cmp.l	a1, d0
-	bhs		Shellsort_For_Loop2		; if cursor + size >= end, break
+	move.l	a1, a0
+	add.l	8(fp), a0				; a0 = cursor = start + size
 
-	move.l	d0, a0					; cursor += size
-	bra		Shellsort_For_Loop1
+Shellsort_OuterFor_Loop:
+	move.l	a0, a3					; a3 = origin = cursor
 
-Shellsort_For_Loop2:
-	move.l	a0, d0
-	sub.l	8(fp), d0
-	cmp.l	4(fp), d0
-	blo		Shellsort_For_Next		; if cursor - size < address, break
+Shellsort_OuterFor_Loop_While:
+	cmp.l	a1, a0					; is cursor <= start? break
+	bls		Shellsort_OuterFor_Loop_While_End
 
-	move.l	d0, a2					; a2 = left = cursor - size
+	sub.l	8(fp), a0
+	move.b	(a0), d0
+	add.l	8(fp), a0				; *(cursor - size)
 
-	move.b	(a2), d0
-	cmp.b	(a0), d0
-	bls		Shellsort_For_Loop2_Next ; if *left <= *cursor, skip next step
+	move.b	(a0), d2
+	cmp.b	d0, d2					; if *cursor >= *(cursor - size)? break
+	bhs		Shellsort_OuterFor_Loop_While_End
 
-	move.b	(a2), d0				; move *left to d0
-	move.b	(a0), (a2)				; overwrite *left with *cursor
-	move.b	d0, (a0)				; overwrite *cursor with d0, original *left
+	sub.l	8(fp), a0
+	move.b	d2, (a0)
+	add.l	8(fp), a0
+	move.b	d0, (a0)				; Swap values at *cursor and *(cursor - size)
 
-Shellsort_For_Loop2_Next:
-	move.l	a2, a0					; cursor = left
-	bra		Shellsort_For_Loop2
+Shellsort_OuterFor_Loop_While_End:
+	move.l	a3, d0
+	add.l	8(fp), d0				; origin + size
+	cmp.l	a2, d0					; if origin + size >= end, break
+	bhs		Shellsort_OuterFor_Loop_End
 
-Shellsort_For_Next:
-	add.l	#1, d1					; i++
+	move.l	d0, a0					; cursor = origin + size
+	bra		Shellsort_OuterFor_Loop
+
+Shellsort_OuterFor_Loop_End:
+	addi.l	#1, d1					; i++
 	move.l	8(fp), d0
 	subi.l	#1, d0					; size - 1
-	cmp.l 	d0, d1
-	bls		Shellsort_For_0ToSizeMinus1 ; next if i is <= size - 1
+	cmp.l	d0, d1
+	bls		Shellsort_OuterFor		; Keep going if i <= size - 1
 
 	bra		Shellsort_While_SizeNot0 ; Next iteration of the while loop
 
 Shellsort_End:
+	move.l	(sp)+, d2
+	move.l	(sp)+, a4
+	move.l	(sp)+, a3
 	move.l	(sp)+, a2
 	RestoreFramePointer
 	rts
